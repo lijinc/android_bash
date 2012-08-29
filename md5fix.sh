0d34158d61bf4134c0b6bab237a45b745c7e9907
liji <liji@liji-Inspiron-N5110.(none)>
2012-08-29 11:07:53 +0530

    removed wildcards to run on android ie script manager

--- md5fix.sh
+++ md5fix.sh
@@ -2,19 +2,29 @@
 echo 'It will fix md5 mismatch error...'
 sleep 1
 echo 'Use it at your own risk...'
-echo -ne '.....                     (33%)\r'
-rm nandroid.md5
-md5sum *.[!s][!h]* > temp1
-echo -ne '.............             (66%)\r'
-md5sum .*.[!s][!h]* > temp2
-cat temp1 temp2 > nandroid.md5
 sleep 1
-echo -ne '..........................(100%)\r'
-rm temp1
-rm temp2
-echo -ne '\n'
-echo done..
-echo by lj@xda
+echo 'Choose the backup to fix'
+cd sdcard/clockworkmod/backup/
+ls -d */ > fl
+awk '{printf "%s.\t%s\n",NR,$0}' fl
+read ch
+cd "`head -$ch fl | tail -1`"
+echo 'wait..'
+md5sum cache.yaffs2.img > ca
+md5sum data.yaffs2.img > da
+md5sum sd-ext.*.tar > sd
+md5sum system.yaffs2.img > sy
+md5sum .android_secure.vfat.tar > sec
+cat ca da sd sy sec > nandroid.md5
+rm ca
+rm da
+rm sd
+rm sy
+rm sec
+cd ..
+rm fl
+echo 'done..'
+echo 'by lj@xda'
 
 
 
